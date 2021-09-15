1ed89baa99df34dc3d966ecd4b7f83199d520c2a
Jerry Chang <therealjerrycan@gmail.com>
2021-09-14 20:28:25 -0700

    Deploy success

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,10 +1,10 @@
 export default (req, res) => {
-  res.status(500).json({
-    text: 'error',
-    version: 4
-  });
-  //res.status(200).json({
-    //text: 'success',
-    //version: 2
+  //res.status(500).json({
+    //text: 'error',
+    //version: 4
   //});
+  res.status(200).json({
+    text: 'success',
+    version: 3
+  });
 }
